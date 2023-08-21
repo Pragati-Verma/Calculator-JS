26af9e70025bc59139944e44eef0d79bcf31193c
Pragati-Verma <30720974+Pragati-Verma@users.noreply.github.com>
2023-08-21 23:00:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+function modulo(num1, num2) {
+    return num1 % num2;
+}
+
+function calculator(num1, num2, operation) {
+    return operation(num1, num2);
+}
